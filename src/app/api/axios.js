7ffc8b9f9a064354ac2fbe04ec36d6b41d4937a4
Vaibhav <vp0303739@gmail.com>
2026-04-05 20:44:29 +0530

    fix: update VITE_API_URL and adjust axios instance formatting

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://delivix-backend-fullstact.onrender.com/api/v1',
   withCredentials: true,
-   credentials: true,
+  credentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
